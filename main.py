d9fcc35042dac225964aab4d3e864a3a340947ee
crsithian monsalve <cristhian.monsalve.2019pob.edu.co>
2019-02-26 13:56:23 -0500

    mi primer commit

---
+++ main.py
@@ -0,0 +1,7 @@
+a=int(input("por favor ingrese el primero numero"))
+c=int(input("por favor ingrese el segundo numero"))
+r=a*c
+r2=2*a
+print("el producto es:{} ".format(r))
+print("el valor doble de a es:{} ".format(r2))      
+      
