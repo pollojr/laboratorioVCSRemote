9fc2f425bc56760bb79d8b4096b7a5628575e688
crsithian monsalve <cristhian.monsalve.2019pob.edu.co>
2019-02-26 14:53:09 -0500

    solucion

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import math
-b=int(input("por favor ingrese el primero numero"))
-d=int(input("por favor ingrese el segundo numero"))
-z=b*b
-raiz= math.sqrt(d)
+a=int(input("ingresa el primer numero"))
+b=int(input("ingresa el segundo numero"))
+c=int(input("ingresa el tercer numero"))
 
-print("el doble de b es: {}" .format(z))
-print("la raiz de d es: {}" .format(raiz))
-
-
-
-
+d=(b*b-4*a*c)
+if a!=0:
+     if d<0:
+         print(" las raices son imaginarias")
+     else:
+         x1=(-b+(math.sqrt(d)))/(2*a)
+         x2=(-b-(math.sqrt(d)))/(2*a)
+         print("x1 = {} x2= {}".format(x1,x2))
+else:
+    print("coefeiciente a debe ser diferente de 0")
+    
+               
+               
